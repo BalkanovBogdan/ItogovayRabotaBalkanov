76f05f263772e813c9604b8b3e64f7888149030b
Балканов Богдан <89509494919@mail.ru>
2023-03-28 02:05:46 +0300

    Создание масива

--- Program.cs
+++ Program.cs
@@ -8,3 +8,13 @@ WriteLine("Ведите строки массива через Enter");
 string [] array = Masiv(a);
 WriteLine("Вывод");
 WriteLine(String.Join("\n",Metod(array)));
+
+string [] Masiv(int index)
+{
+    string [] res = new string[index];
+    for(int i = 0; i < index; i++)
+    {
+        res[i] = ReadLine()!;
+    }
+    return res;
+}
