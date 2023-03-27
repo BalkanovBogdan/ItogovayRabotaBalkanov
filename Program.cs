fb64aac0f989c76e512b13c028537e6635860917
Балканов Богдан <89509494919@mail.ru>
2023-03-28 02:05:13 +0300

    ввод данных

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите длину массива");
+int a = int.Parse(ReadLine()!);
+WriteLine("Ведите строки массива через Enter");
+string [] array = Masiv(a);
+WriteLine("Вывод");
+WriteLine(String.Join("\n",Metod(array)));
